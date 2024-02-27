43f8be873c97ed01a130706197e366d1734729bd
tuan anh <tuananhtran.xd@gmail.com>
2024-02-27 13:30:00 +0700

    first commit

---
+++ src/main/java/controller/EmailController.java
@@ -0,0 +1,40 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+@Controller
+public class EmailController {
+    public static final String EMAIL_REGEX = "^[A-Za-z0-9]+[A-Za-z0-9]*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)$";
+    private Pattern pattern;
+
+    public EmailController() {
+        pattern = Pattern.compile(EMAIL_REGEX);
+    }
+    @GetMapping("/home")
+    public String home(){
+        return "home";
+    }
+    @PostMapping("/validate")
+    public String user(@RequestParam("email") String email, ModelMap modelMap){
+        boolean isValid = this.validate(email);
+        if (!isValid){
+            modelMap.addAttribute("message", "Email is invalid");
+            return "home";
+        }
+        modelMap.addAttribute("email", email);
+        return "success";
+    }
+
+    private boolean validate(String email) {
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+
+}
